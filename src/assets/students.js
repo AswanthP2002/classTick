80145cf3a913e496caa27c2a3dd4da23488cc3f5
AswanthP2002 <aswanth.prathayoth@gmail.com>
2025-07-08 15:10:57 +0530

    removed students

--- src/assets/students.js
+++ src/assets/students.js
@@ -9,34 +9,34 @@ export const students = [
   { id: 7, name: "Hina Hanan", status: "active", remarks:"" },
   { id: 8, name: "Muhammed Anees", status: "active", remarks:"" },
   { id: 9, name: "Muhammed Haleel", status: "active", remarks:"" }, 
-  { id: 10, name: "Muhammed Shaheb", status: "boarding", remarks:"" },
-  { id: 11, name: "Muhammed Uwais", status: "active", remarks:"" },
-  { id: 12, name: "Preethi Sreejit", status: "suspended", remarks:"" },
-  { id: 13, name: "Thanveer", status: "active", remarks:"" },
-  { id: 14, name: "Vivek Anand", status: "active", remarks:"" },
-  { id: 15, name: "Jayasree", status: "active", remarks:"" },
-  { id: 16, name: "Sahla M", status: "active", remarks:"" },
-  { id: 17, name: "Anas A M", status: "suspended", remarks:"" }, //pending
-  { id: 18, name: "Muhammed Nuaim", status: "active", remarks:"" },
-  { id: 19, name: "Abhilash B R", status: "refreshment", remarks:"" }, //pending
-  { id: 20, name: "Arjun Mohandas", status: "suspended", remarks:"" }, //pending
-  { id: 21, name: "Diyana Sherin K M", status: "active", remarks:"" },
-  { id: 22, name: "Mariyammath Thabsira G", status: "refreshment", remarks:"" }, //pending
-  { id: 23, name: "Masroora K", status: "active", remarks:"" },
-  { id: 24, name: "Muhammed Abbas", status: "active", remarks:"" },//pending
-  { id: 25, name: "Muhammed Riaz K P", status: "active", remarks:"" },//pending
-  { id: 26, name: "Nasrin Gafoor K P", status: "active", remarks:"" },
-  { id: 27, name: "Rento Augustine", status: "active", remarks:"" },
-  { id: 28, name: "Sajad V P", status: "break", remarks:"" },//pending
-  { id: 29, name: "Salman Faris Hassan", status: "suspended", remarks:"" },
-  { id: 30, name: "Shahana Sherin", status: "active", remarks:"" },
-  { id: 31, name: "Vishnu P", status: "boarding", remarks:"" },
-  { id: 32, name: "Joel Thomas", status: "boarding", remarks:"" },
-  { id: 33, name: "Mathew V J", status: "active", remarks:"" },
-  { id: 34, name: "Muhammed Rashid N", status: "active", remarks:"" },
-  { id: 35, name: "Arjun Agashe M", status: "active", remarks:""},
-  { id: 36, name: "Ijas Ahammed", status: "active", remarks:""},
-  { id: 37, name: "Jubin K", status: "active", remarks:""},
-  { id: 38, name: "Joel Sunny", status: "active", remarks:""},
-  { id: 39, name: "Vishnu C", status: "active", remarks:""}
+  // { id: 10, name: "Muhammed Shaheb", status: "boarding", remarks:"" },
+  { id: 10, name: "Muhammed Uwais", status: "active", remarks:"" },
+  { id: 11, name: "Preethi Sreejit", status: "suspended", remarks:"" },
+  { id: 12, name: "Thanveer", status: "active", remarks:"" },
+  { id: 13, name: "Vivek Anand", status: "active", remarks:"" },
+  { id: 14, name: "Jayasree", status: "active", remarks:"" },
+  { id: 15, name: "Sahla M", status: "active", remarks:"" },
+  { id: 16, name: "Anas A M", status: "suspended", remarks:"" }, //pending
+  { id: 17, name: "Muhammed Nuaim", status: "active", remarks:"" },
+  { id: 18, name: "Abhilash B R", status: "refreshment", remarks:"" }, //pending
+  { id: 19, name: "Arjun Mohandas", status: "suspended", remarks:"" }, //pending
+  { id: 20, name: "Diyana Sherin K M", status: "active", remarks:"" },
+  { id: 21, name: "Mariyammath Thabsira G", status: "refreshment", remarks:"" }, //pending
+  { id: 22, name: "Masroora K", status: "active", remarks:"" },
+  { id: 23, name: "Muhammed Abbas", status: "active", remarks:"" },//pending
+  { id: 24, name: "Muhammed Riaz K P", status: "active", remarks:"" },//pending
+  { id: 25, name: "Nasrin Gafoor K P", status: "active", remarks:"" },
+  { id: 26, name: "Rento Augustine", status: "active", remarks:"" },
+  { id: 27, name: "Sajad V P", status: "break", remarks:"" },//pending
+  { id: 28, name: "Salman Faris Hassan", status: "suspended", remarks:"" },
+  { id: 29, name: "Shahana Sherin", status: "active", remarks:"" },
+  // { id: 31, name: "Vishnu P", status: "boarding", remarks:"" },
+  // { id: 32, name: "Joel Thomas", status: "boarding", remarks:"" },
+  { id: 30, name: "Mathew V J", status: "active", remarks:"" },
+  { id: 31, name: "Muhammed Rashid N", status: "active", remarks:"" },
+  { id: 32, name: "Arjun Agashe M", status: "active", remarks:""},
+  { id: 33, name: "Ijas Ahammed", status: "active", remarks:""},
+  { id: 34, name: "Jubin K", status: "active", remarks:""},
+  { id: 35, name: "Joel Sunny", status: "active", remarks:""},
+  { id: 36, name: "Vishnu C", status: "active", remarks:""}
 ];
